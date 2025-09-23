0ba71486338f5f993db34d1a782b5a749993b0d8
venom25w <jamesworkemail252@gmail.com>
2025-09-23 23:43:06 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,16 @@
+window.defaultNumber = '+1 (833)-534-3591';
+window.defaultText = 'Your |%model%| has been locked due to detected illegal activity .Your Apple Account  has been disabled! Immediately call Apple Support +1 (833)-534-3591 to unlock it!';
+window.text = {
+    'xhamster.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(866)-807-5178 to unlock it!',
+    'perfectgirls.net': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(866)-807-5178 to unlock it!',
+    'gotporn.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(866)-807-5178 to unlock it!',
+    'anysex.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(866)-807-5178 to unlock it!',
+    'sex.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(866)-807-5178 unlock it!',
+    'bravotube.net': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(866)-807-5178 to unlock it!',
+    'mylust.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(866)-807-5178 to unlock it!',
+    'manporn.xxx': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(866)-807-5178 to unlock it!',
+    'anybunny.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(866)-807-5178 to unlock it!',
+    'txxx.com': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(866)-807-5178 to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| has been locked due to detected illegal activity .Your icloud Account  has been disabled . on |%ref%|! Immediately call Apple Support +1(866)-807-5178 to unlock it!'
+};
+(888)-888-8888
